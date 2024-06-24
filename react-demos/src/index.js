a2fe5c5972b21f05de466e9b5fca9170e9d368e1
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-06-24 22:21:27 +0200

    Added basic login/logout functionality with Auth0

--- react-demos/src/index.js
+++ react-demos/src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './static/css/output.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <NextUIProvider>
-    <App />
-  </NextUIProvider>
-  // </React.StrictMode>
+  <Auth0Provider
+    domain="dev-do7e3my01rhnrak7.eu.auth0.com"
+    clientId="o2AsMGQI8tqlJoEb1HOVaMNR6oZqGYXJ"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </Auth0Provider>
 );
 
 reportWebVitals();
