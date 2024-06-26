e46685e92dc343a1b0e8400040acc4714980e08b
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-06-26 14:26:30 +0200

    Added NotFound component

--- react-demos/src/App.js
+++ react-demos/src/App.js
@@ -1,6 +1,7 @@
 import './static/css/App.css';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Navigation from './Components/Navigation';
+import NotFound from './Components/NotFound';
 import ProductPortfolio from './Views/ProductPortfolio';
 import Home from './Views/Home';
 import SpaceGame from './Views/SpaceGame';
@@ -17,9 +18,11 @@ function App() {
             <Route exact path='/productportfolioexample' element={<ProductPortfolio />} />
             <Route exact path='/spacegameexample' element={<SpaceGame />} />
             <Route exact path='/' element={<Home />} />
+            <Route exact path='/home' element={<Home />} />
             <Route exact path='/about' element={null} />
             <Route exact path='/contact' element={null} />
             <Route exact path='/profile' element={<Profile />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
     </div>
--- react-demos/src/Views/Home.js
+++ react-demos/src/Views/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <h1 className="text-2xl font-bold underline">
-      
+      Test
     </h1>
   )
 }
