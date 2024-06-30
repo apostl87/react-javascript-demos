93fb34dc1915228efc679909ada676493e9a72ff
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-06-30 14:46:52 +0200

    Added cors

--- node-postgres/api/index.js
+++ node-postgres/api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const ProductModel = require('./Models/ProductModel.js');
 const CountryModel = require('./Models/CountryModel.js');
 
@@ -6,8 +7,13 @@ const app = express()
 const port = 3001
 
 app.use(express.json())
+
+// Enable CORS for cross-origin requests
+app.use(cors());
+
 app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	// res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	// res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3002');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
 	next();
