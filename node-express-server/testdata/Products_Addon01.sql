bd2c63fba37e45a61a15997fef0734d99c1f062c
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-07-03 01:02:29 +0200

    repaired gitignore 2nd time

--- node-express-server/testdata/Products_Addon01.sql
+++
@@ -1,7 +0,0 @@
--- Add a new column for image URLs if it doesn't exist
-ALTER TABLE products ADD COLUMN image_url VARCHAR(255);
-
--- Update each product with a random image URL
-UPDATE products
-SET image_url = CONCAT('https://picsum.photos/seed/', FLOOR(RANDOM() * 1000), '/200/300')
-WHERE image_url IS NULL OR image_url = '';
--- node-express-server/testdata/connect-to-db.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-psql -d my_database -U postgres
