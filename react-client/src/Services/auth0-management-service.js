d678fb66a4688e53886ff87e2e6d3752105f13c7
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-07-12 22:22:06 +0200

    Added delete endpoint to Auth0 management service

--- react-client/src/Services/auth0-management-service.js
+++ react-client/src/Services/auth0-management-service.js
@@ -17,17 +17,17 @@ export const getUser = async (id) => {
     };
 
     return axios.request(config)
-    .then((response) => {
-        //console.log(response)
-        return (response)
-    })
-    .catch((error) => {
-        console.log(error);
-        return (error.response)
-    });
+        .then((response) => {
+            //console.log(response)
+            return (response)
+        })
+        .catch((error) => {
+            console.log(error);
+            return (error.response)
+        });
 }
 
-export const updateUser = async (id, userData, onSuccess) => {
+export const updateUser = async (id, userData) => {
     let data = JSON.stringify(userData);
 
     let config = {
@@ -43,12 +43,35 @@ export const updateUser = async (id, userData, onSuccess) => {
     };
 
     return axios.request(config)
-    .then((response) => {
-        // console.log(response)
-        return (response)
-    })
-    .catch((error) => {
-        console.log(error);
-        return (error.response)
-    });
+        .then((response) => {
+            // console.log(response)
+            return (response)
+        })
+        .catch((error) => {
+            console.log(error);
+            return (error.response)
+        });
+}
+
+export const deleteUser = async (id) => {
+
+    let config = {
+        method: 'delete',
+        maxBodyLength: Infinity,
+        url: `https://${base_url}/api/v2/users/${id}`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${apiToken}`,
+        },
+    };
+
+    return axios.request(config)
+        .then((response) => {
+            return (response)
+        })
+        .catch((error) => {
+            console.log(error);
+            return (error.response)
+        });
 }
