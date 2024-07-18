a19b1a5d1cde96aa66b685c834d83f9b0999bda3
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-07-18 03:30:27 +0200

    Added comment on non-protected API on Home

--- react-client/src/Views/Home.js
+++ react-client/src/Views/Home.js
@@ -4,6 +4,10 @@ const Home = () => {
   return (
     <div className='p-5'>
       This is a small demonstration page utilizing a database CRUD application and basic pure JavaScript applications.
+      <br />
+      <br />
+      Note that the API is protected JWT Bearer Token Authentication, however, since limited quota for tokens is available at my hosting provider,
+      Token Authentication can be disabled from time to time.
     </div>
   )
 }
