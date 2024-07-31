5ce679e4fb6b1074b19558e4c89f96834418004b
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-07-31 16:41:33 +0200

    Added automatic refresh of management API access token

--- react-client/src/Services/auth0-management-service.js
+++ react-client/src/Services/auth0-management-service.js
@@ -1,30 +1,32 @@
 import axios from 'axios';
 import React from 'react';
 
-let baseUrl = process.env.REACT_APP_AUTH0_DOMAIN;
-let apiToken = process.env.REACT_APP_AUTH0_MNGMT_TOKEN;
+// Service for the Auth0 Management API
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const audience = "https://" + domain + "/api/v2"
+const client_id = process.env.REACT_APP_AUTH0_API_M2M_CLIENT_ID;
+const client_secret = process.env.REACT_APP_AUTH0_API_M2M_CLIENT_SECRET;
+
+let token_varname = 'react-demos_auth0-management-token';
+
+const getApiToken = () => {
+    return localStorage.getItem(token_varname)
+}
 
 export const getUser = async (id) => {
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: `https://${baseUrl}/api/v2/users/${id}`,
+        url: `https://${domain}/api/v2/users/${id}`,
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': `Bearer ${apiToken}`,
         }
     };
 
-    return axios.request(config)
-        .then((response) => {
-            //console.log(response)
-            return (response)
-        })
-        .catch((error) => {
-            console.log(error);
-            return (error.response)
-        });
+    let res = await axiosRequest(config);
+    return res;
 }
 
 export const updateUser = async (id, userData) => {
@@ -33,44 +35,87 @@ export const updateUser = async (id, userData) => {
     let config = {
         method: 'patch',
         maxBodyLength: Infinity,
-        url: `https://${baseUrl}/api/v2/users/${id}`,
+        url: `https://${domain}/api/v2/users/${id}`,
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': `Bearer ${apiToken}`,
         },
         data: data
     };
 
-    return axios.request(config)
-        .then((response) => {
-            // console.log(response)
-            return (response)
-        })
-        .catch((error) => {
-            console.log(error);
-            return (error.response)
-        });
+    let res = await axiosRequest(config);
+    return res;
 }
 
 export const deleteUser = async (id) => {
     let config = {
         method: 'delete',
         maxBodyLength: Infinity,
-        url: `https://${baseUrl}/api/v2/users/${id}`,
+        url: `https://${domain}/api/v2/users/${id}`,
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': `Bearer ${apiToken}`,
         },
     };
 
-    return axios.request(config)
+    let res = await axiosRequest(config);
+    return res;
+}
+
+const axiosRequest = async (config) => {
+    let apiToken = getApiToken();
+    let hasNewToken = false;
+
+    // Getting new API token if none is set in localStorage
+    if (!apiToken) {
+        await refreshToken();
+        apiToken = getApiToken();
+        hasNewToken = true;
+    }
+
+    config.headers.Authorization = `Bearer ${apiToken}`;
+    console.log(config);
+
+    let res = await axios.request(config)
         .then((response) => {
+            console.log(response);
             return (response)
         })
         .catch((error) => {
             console.log(error);
             return (error.response)
         });
+
+    // Refreshing token once on two error codes: 400 (e.g. no token) and 401 (e.g. expired token)
+    // TODO: Estimate expiry date
+    if ((res.status === 400 || res.status === 401) && hasNewToken == false) {
+        localStorage.removeItem(token_varname)
+        return await axiosRequest(config);
+    } else {
+        return res;
+    }
+}
+
+const refreshToken = async () => {
+
+    let config = {
+        method: 'POST',
+        url: `https://${domain}/oauth/token`,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: new URLSearchParams({
+            grant_type: 'client_credentials',
+            client_id: client_id,
+            client_secret: client_secret,
+            audience: audience,
+        })
+    };
+
+    await axios.request(config).then(function (response) {
+        console.log("Received new Auth0 Management API access token")
+        // console.log("New token: ", response.data.access_token);
+        localStorage.setItem(token_varname, response.data.access_token);
+    }).catch(function (error) {
+        console.error(error);
+    });
+
 }
